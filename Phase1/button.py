6f971edda8c8c2ab9888ca4db1d683fa61e26b48
tylersuchan <tylersuc@buffalo.edu>
2018-11-01 13:52:09 -0400

    Everything for part 1 completed. Will dicuss with Jared today

---
+++ Phase1/button.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import os
+    
+
+def write_slogan():
+    os.system('flask run')
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, 
+                   text="QUIT", 
+                   fg="red",
+                   command=quit)
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,
+                   text="Hello",
+                   command=write_slogan)
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
---
+++ Phase1/hello.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import docker
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    client = docker.from_env()
+    return client.containers.run('hello-world')
+    #return 'Hello, World!'
