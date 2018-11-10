1f410821a4f2bf3b08c241f42cf3a9b1c7bdb8eb
tylersuchan <tylersuc@buffalo.edu>
2018-11-10 16:10:18 -0500

    Added checkboxes and verified that server detects button clicks

--- Phase2/main.py
+++ Phase2/main.py
@@ -3,6 +3,11 @@ import docker
 import os
 app = Flask(__name__, static_url_path='')
 
+broker = {"saiyans": "",
+            "namekians": "",
+            "earthlings": "",
+            "villians": ""}
+
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
@@ -10,6 +15,11 @@ def send_js(path):
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('css', path)
+
+
+@app.route('/')
+def result():
+    return render_template("pubsub.html")
 #
 # @app.route('/publish')
 # def result2():
@@ -20,13 +30,15 @@ def send_css(path):
 @app.route('/', methods=['GET', 'POST'])
 def getUserInput():
     # Get user input from front end
-    text = request.form.get('fighterz')
-    subscribe(text)
-    return render_template("pubsub.html", output=str(text))
-
-def subscribe(text):
-    return render_template("pubsub.html", output=str(text))
+    # text = request.form['box']
+    # dispInput(text)
+    if (request.form.get('saiyan') == 'saiyan'):
+        print("saiyan is selected")
+    if (request.form.get('namekian') == 'namekian'):
+        print("namekian is selected")
+    return render_template("pubsub.html")
 
 
 if __name__ == '__main__':
    app.run(debug = True, host="0.0.0.0")
+
